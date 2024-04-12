84cd39c76e7fb2b1fa3313a7fa64cb10d92f8446
fakaha <lunastra34@gmail.com>
2024-04-12 15:20:36 +0700

    feat: add function generateID, function generateBookObject, event NGERENDER_EVENT

--- main.js
+++ main.js
@@ -7,7 +7,7 @@
 // }
 
 const books = [];
-const RENDER_EVENT = "render-book";
+const NGERENDER_EVENT = "render-book";
 
 document.addEventListener("DOMContentLoaded", function () {
     const submitForm = document.getElementById("inputBook");
@@ -35,6 +35,53 @@ function addBook(){
     );
     books.push(bookObject);
 
-    document.dispatchEvent(new Event(RENDER_EVENT));
+    document.dispatchEvent(new Event(NGERENDER_EVENT));
     alert(`Data buku berhasil ditambahkan!`)
+}
+
+function generateId() {
+  return +new Date();
+}
+
+function generateBookObject(id, title, author, year, isComplete){
+  return{
+    id, 
+    title, 
+    author, 
+    year, 
+    isComplete
+  };
+}
+
+document.addEventListener(NGERENDER_EVENT, function(){
+  console.log(books);
+
+  const uncompletedBooksList = document.getElementById('incompleteBookshelfList');
+  incompleteBookshelfList = '';
+
+  const completedBooksList = document.getElementById('completeBookshelfList');
+  completedBooksList = '';
+
+  for(const book of books){
+    const makeBook = makeBookShelf(book);
+
+    if(makeBook.isComplete === true){
+      completedBooksList.append(makeBook);
+    }else{
+      uncompletedBooksList.append(makeBook);
+    }
+  }
+
+})
+
+function makeBookShelf(bookObject){
+// <article class="book_item">
+//  <h3>Book Title</h3>
+//  <p>Penulis: John Doe</p>
+//  <p>Tahun: 2002</p>
+//  <div class="action">
+//    <button class="green">Selesai dibaca</button>
+//    <button class="red">Hapus buku</button>
+//  </div>
+// </article>
 }
