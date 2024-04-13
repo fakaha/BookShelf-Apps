08498606ca9a5c71779e53dcaff24812fa7f0d28
fakaha <lunastra34@gmail.com>
2024-04-13 11:21:21 +0700

    feat: add make bookshlef

--- main.js
+++ main.js
@@ -75,13 +75,65 @@ document.addEventListener(NGERENDER_EVENT, function(){
 })
 
 function makeBookShelf(bookObject){
-// <article class="book_item">
-//  <h3>Book Title</h3>
-//  <p>Penulis: John Doe</p>
-//  <p>Tahun: 2002</p>
-//  <div class="action">
-//    <button class="green">Selesai dibaca</button>
-//    <button class="red">Hapus buku</button>
-//  </div>
+  // <article class="book_item">
+  const container = document.createElement('article');
+  container.classList.add('book_item');
+
+  //  <h3>Book Title</h3>
+  const textTitle = document.createElement('h3');
+  textTitle.innerText = bookObject.bookTitle;
+
+  //  <p>Penulis: John Doe</p>
+  const textAuthor = document.createElement('p');
+  textAuthor.innerText = bookObject.author;
+
+  //  <p>Tahun: 2002</p>
+  const textYear = document.createElement('p');
+  textYear.innerText = bookObject.year;
+
+  //  <div class="action">
+  //    <button class="green">Selesai dibaca</button>
+  //    <button class="red">Hapus buku</button>
+  //  </div>
+  const containerAction = document.createElement('div');
+  containerAction.classList.add('action');
+
+  container.append(textTitle, textAuthor, textYear, containerAction);
+
+  if(bookObject.isComplete){
+    const incompleteButton = document.createElement('button');
+    incompleteButton.classList.add('green');
+    incompleteButton.innerText = 'Belum selesai di Baca';
+
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add('red');
+    deleteButton.innerText = 'Hapus buku';
+
+    containerAction.append(incompleteButton, deleteButton);
+  }else{
+    const doneButton = document.createElement('button');
+    doneButton.classList.add('green');
+    doneButton.innerText = 'Selesai dibaca';
+
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add('red');
+    deleteButton.innerText = 'Hapus buku';
+
+    containerAction.append(doneButton, deleteButton);
+  }
+  return container;
 // </article>
+}
+
+function findBook(bookID){
+  for(const bookItem of books){
+    if(bookItem.id === bookID){
+      return bookItem
+    }
+  }
+  return null;
+}
+
+function addBookToCompleted(bookID){
+  
 }
